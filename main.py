8454ef292572a6c18d336e88b3a294c2162b4a4f
Remi Sha <105662433+cestnana@users.noreply.github.com>
2024-10-07 04:34:54 +0800

    main complete code (test passed)

--- main.py
+++ main.py
@@ -2,40 +2,42 @@
 import math
 
 def is_composite(num):
-  # num_square = math.sqrt(num)
-  # int_num_sqt = math.ceil(num_square)
-  # for element in range(1, int_num_sqt):
-  #   divided_count = 0
+  
+  if num == 1 or num == 2:
+    return False
+  
+  if num == 4:
+    return True
+  
   divided_count = 0
-  while divided_count < 4:
-    num_square = math.sqrt(num)
-    print(num_square)
-    int_num_sqt = math.ceil(num_square)
-    for element in range(1, int_num_sqt, 1):
-      # divided_count = 0
-      
-      is_divided = num % element
-      if is_divided == 0:
-        divided_count += 1
-        
-      element += 1
-      print(f"divided count: {divided_count}, current element is: {element}, int num sqt: {int_num_sqt}.")
-  return True
+  num_square = math.sqrt(num)
+  # print(num_square)
+  int_num_sqt = math.ceil(num_square)
+  for element in range(2, int_num_sqt, 1):
+    is_divided = num % element
+    if is_divided == 0:
+      divided_count += 1
+  # print(f"divided count: {divided_count}, current element is: {element}, int num sqt: {int_num_sqt}.")
+  
+  if divided_count == 0:
+    return False
+  else:
+    return True
+  
 
 def is_prime(num):
-  if num != 1 or is_composite(num) == False:
+  if is_composite(num) == False and num != 1:
     return True
   else:
     return False
-        
+
 print(is_prime(73))
 # except: True
-
 print(is_prime(75))
 # except: False
-
 print(is_prime(1))
 # except: False
-
 print(is_prime(2))
 # except: True
+print(is_prime(4))
+# except: False
